a44c1f51c94b181a18f11a68fa619b5be9c94a30
Marco <marco.dmail96@gmail.com>
2025-03-14 10:48:39 +0100

    Create deleteUserById.ts

---
+++ server/src/services/users/deleteUserById.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const deleteUserById = async (id: string) => {
+  const prisma = new PrismaClient();
+  const user = await prisma.user.deleteMany({
+    where: { id },
+  });
+  return user.count > 0 ? id : null;
+};
+
+export default deleteUserById;
