b6268578d30e4e1b57b5cbd4107b73beb05a8505
Marco <marco.dmail96@gmail.com>
2025-03-14 10:49:04 +0100

    Update users router

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,22 +1,12 @@
 import { Router, Request, Response, NextFunction } from "express";
-import { body } from "express-validator";
-import validateResource from "../middlewares/validationMiddleware.ts";
 import getUsers from "../services/users/getUsers.ts";
-import notFoundErrorHandler from "../middlewares/notFoundErrorHandler.ts";
 import getUserById from "../services/users/getUserById.ts";
 import createUser from "../services/users/createUser.ts";
 import updateUserById from "../services/users/updateUserById.ts";
+import deleteUserById from "../services/users/deleteUserById.ts";
+import validateFields from "../middlewares/validationMiddleware.ts";
 
 const router = Router();
-
-// Define type for "User"
-interface User {
-  username: string;
-  password: string;
-  name: string;
-  email: string;
-  phoneNumber: string;
-}
 
 // GET all users
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
@@ -41,27 +31,12 @@ router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
   } catch (error) {
     next(error);
   }
-  notFoundErrorHandler;
 });
 
 // POST route to create a new user
 router.post(
   "/",
-  validateResource<User>({
-    username: [body("username").notEmpty().withMessage("Username is required")],
-    password: [
-      body("password")
-        .isLength({ min: 8 })
-        .withMessage("Password must be at least 8 characters"),
-    ],
-    email: [body("email").isEmail().withMessage("Invalid email address")],
-    name: [body("name").notEmpty().withMessage("Name is required")],
-    phoneNumber: [
-      body("phoneNumber")
-        .isMobilePhone("any")
-        .withMessage("Invalid phone number"),
-    ],
-  }),
+  validateFields(["username", "password", "name", "email", "phoneNumber"]),
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { username, password, name, email, phoneNumber } = req.body;
@@ -84,10 +59,7 @@ router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { username, password, name, email, phoneNumber } = req.body;
     const { id } = req.params;
-    /* Verifica se i dati richiesti sono validi
-    if (!username || !password || !name || !email || !phoneNumber) {
-      return res.status(400).json({ message: "Missing required fields" });
-    }*/
+
     const user = await updateUserById(id, {
       username,
       password,
@@ -109,4 +81,23 @@ router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
   }
 });
 
+//DELETE user by Id
+router.delete(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const user = await deleteUserById(id);
+      if (user) {
+        res
+          .status(200)
+          .send({ message: `User with id ${id} successfully deleted!`, user });
+      } else {
+        res.status(404).json({ message: `User with id ${id} not found` });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 export default router;
