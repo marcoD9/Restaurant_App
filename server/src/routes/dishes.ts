b514af755fef2063b0da6420a0ba96a70a440947
Marco <marco.dmail96@gmail.com>
2025-03-17 21:23:52 +0100

    Create dishes router

---
+++ server/src/routes/dishes.ts
@@ -0,0 +1,96 @@
+import { Router, Request, Response, NextFunction } from "express";
+import validateFields from "../middlewares/validationMiddleware.ts";
+import getDishes from "../services/dishes/getDishes.ts";
+import getDishById from "../services/dishes/getDishById.ts";
+import createDish from "../services/dishes/createDish.ts";
+import updateDishById from "../services/dishes/updateDishById.ts";
+import deleteDishById from "../services/dishes/deleteDishById.ts";
+
+const router = Router();
+
+// GET all dishes
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(req.query); // Use req for debug purpose
+    const dishes = await getDishes();
+    res.json(dishes);
+  } catch (error) {
+    next(error);
+  }
+});
+// GET dishes by Id
+router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    const dish = await getDishById(id);
+    if (!dish) {
+      res.status(404).json({ message: `Dish with ${id} not found!` });
+    } else {
+      res.status(200).json(dish);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST route to create a new dish
+router.post(
+  "/",
+  validateFields(["name", "description", "price", "image"]),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, description, price, image } = req.body;
+      const newDish = await createDish(name, description, price, image);
+      res.status(201).json(newDish);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//PUT to update dish by Id
+router.put("/:id", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, description, price, image } = req.body;
+    const { id } = req.params;
+
+    const dish = await updateDishById(id, {
+      name,
+      description,
+      price,
+      image,
+    });
+    if (dish) {
+      res
+        .status(200)
+        .send({ message: `Dish with id ${id} has been updated`, dish });
+    } else {
+      res
+        .status(404)
+        .json({ message: `Dish with id ${id} has not been found` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE dish by Id
+router.delete(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const dish = await deleteDishById(id);
+      if (dish) {
+        res
+          .status(200)
+          .send({ message: `Dish with id ${id} successfully deleted!`, dish });
+      } else {
+        res.status(404).json({ message: `Dish with id ${id} not found` });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+export default router;
