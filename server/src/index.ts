045799a02dde9cfc98952c419a0e7c9654869a40
Marco <marco.dmail96@gmail.com>
2025-03-21 10:19:43 +0100

    Update index.ts, add login router

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import usersRouter from "./routes/users.ts";
 import dishesRouter from "./routes/dishes.ts";
 import ordersRouter from "./routes/orders.ts";
 import orderDishesRouter from "./routes/orderDishes.ts";
+import loginRouter from "./routes/login.ts";
 import "dotenv/config";
 import errorHandler from "./middlewares/errorHandler.ts";
 import log from "./middlewares/logMiddleware.ts";
@@ -13,6 +14,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(log);
 
+app.use("/login", loginRouter);
 app.use("/users", usersRouter);
 app.use("/dishes", dishesRouter);
 app.use("/orders", ordersRouter);
