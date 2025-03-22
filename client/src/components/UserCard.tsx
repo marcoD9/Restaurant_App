a36c4e67a2aa593bd01e8185c064f5ce518cd874
Marco <marco.dmail96@gmail.com>
2025-03-22 14:17:53 +0100

    Create UserCard to render user datas

---
+++ client/src/components/UserCard.tsx
@@ -0,0 +1,18 @@
+import { Box, Stack, Text } from "@chakra-ui/react";
+import { User } from "../types";
+
+interface UserCardProps {
+  user: User;
+}
+
+function UserCard({ user }: UserCardProps) {
+  return (
+    <Box>
+      <Stack>
+        <Text>User: {user.name}</Text>
+      </Stack>
+    </Box>
+  );
+}
+
+export default UserCard;
