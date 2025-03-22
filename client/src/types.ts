0eddb4e45485078089b26aedd5328add169cc686
Marco <marco.dmail96@gmail.com>
2025-03-22 14:17:16 +0100

    Create type.ts, contains the structure of types

---
+++ client/src/types.ts
@@ -0,0 +1,16 @@
+export interface Dish {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+}
+
+export interface User {
+  id: string;
+  username: string;
+  password: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+}
