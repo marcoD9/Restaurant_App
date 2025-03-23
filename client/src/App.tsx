eb1fbf8d5bb071c5cb650552b28de0b0bde876f8
Marco <marco.dmail96@gmail.com>
2025-03-23 21:57:08 +0100

    Update App, add route for DishDetails

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
+import DishDetails from "./pages/DishDetails.tsx";
 //import About from './pages/About';
 //import Contact from './pages/Contact';
 
@@ -9,9 +10,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dishes/:id" element={<DishDetails id={""} />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
