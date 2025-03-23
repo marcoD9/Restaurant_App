65a3845b8bb18bd0e4eeae4d54e3466a1c0dfbbc
Marco <marco.dmail96@gmail.com>
2025-03-23 21:55:11 +0100

    Update DishCard, add a prop to toggle description rendering

--- client/src/components/DishCard.tsx
+++ client/src/components/DishCard.tsx
@@ -3,15 +3,17 @@ import { Dish } from "../types";
 
 interface DishCardProps {
   dish: Dish;
+  showDescription?: boolean; //  Optional prop to toggle description rendering
 }
 
-function DishCard({ dish }: DishCardProps) {
+function DishCard({ dish, showDescription = false }: DishCardProps) {
   return (
     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" p={4}>
       <Image src={dish.image} alt={dish.name} borderRadius="lg" />
       <Stack mt="6">
         <Heading size="md">{dish.name}</Heading>
-        <Text>{dish.description}</Text>
+        {showDescription ? <Text>{dish.description}</Text> : null}{" "}
+        {/*Render description only if the prop is true*/}
         <Text color="blue.600" fontSize="2xl">
           {dish.price}€
         </Text>
