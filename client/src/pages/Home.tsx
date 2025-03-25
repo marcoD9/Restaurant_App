0affb398997e1b80220b4bd9a33d7db068a742ea
Marco <marco.dmail96@gmail.com>
2025-03-25 15:21:47 +0100

    Update Home, add the Form. Render the user after login

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Box, Heading, Flex, Text } from "@chakra-ui/react";
+import { Box, Heading, Flex, Text, Button } from "@chakra-ui/react";
 import DishCard from "../components/DishCard.tsx";
 import UserCard from "../components/UserCard.tsx";
-import { Dish } from "../types";
-import { User } from "../types";
-import { fetchDish } from "../api";
-import { fetchUser } from "../api";
+import { Dish, LoginResponse, User } from "../types";
+import { fetchDish, fetchUserById } from "../api";
+import LoginForm from "@/components/LoginForm.tsx";
 
 const Home: React.FC = () => {
   const [dishes, setDishes] = useState<Dish[]>([]);
-  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<User | null>(null);
   const [error, setError] = useState<string | null>(null);
-
-  //Fetch Dish
+  const [loginError, setLoginError] = useState<string | null>(null);
+  const [isLoginFormVisible, setIsLoginFormVisible] = useState(false);
+  //Fetch Dishes
   useEffect(() => {
     const loadData = async () => {
       try {
@@ -30,36 +30,40 @@ const Home: React.FC = () => {
     };
     loadData();
   }, []);
-  //Fetch User
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        const data = await fetchUser();
-        setUsers(data);
-        setError(null);
-      } catch (e: unknown) {
-        if (e instanceof Error) {
-          setError(e.message);
-        } else {
-          setError("An unknown error occurred.");
-        }
+  //Fetch User on successfull login
+  const handleLoginSuccess = async (userData: LoginResponse) => {
+    try {
+      const fetchedUser = await fetchUserById(userData.user.id, userData.token);
+      setUser(fetchedUser);
+      setLoginError(null);
+      setIsLoginFormVisible(false); // Hide form after successful login
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setLoginError(error.message);
+      } else {
+        setLoginError("An unknown error occurred.");
       }
-    };
-    loadData();
-  }, []);
+    }
+  };
 
-  if (error) {
-    return <Text color="red.500">Error: {error}</Text>;
-  }
+  const handleLoginError = (error: string) => {
+    setLoginError(error);
+  };
 
   return (
     <div>
-      {/*Dishes*/}
-      <Box className="flex flex-row items-center justify-center min-h-screen bg-gray-100 p-4">
-        <Heading className="text-4xl font-bold mb-8" as="h1" size="2xl">
-          Dishes:
+      {error && <Text color="red">{error}</Text>}
+      {loginError && <Text color="red">{loginError}</Text>}
+      <Box className="center-col">
+        <Heading
+          className="text-4xl text-black font-bold mb-8"
+          as="h1"
+          size="2xl"
+        >
+          Restaurant
         </Heading>
-        <Flex className="flex-row flex-wrap justify-center gap-4 w-full">
+        {/*Dish*/}
+        <Flex className="center-col">
           {dishes.map((dish) => (
             <Link key={dish.id} to={`/dishes/${dish.id}`}>
               <DishCard key={dish.id} dish={dish} />
@@ -67,16 +71,47 @@ const Home: React.FC = () => {
           ))}
         </Flex>
       </Box>
-      {/*Users*/}
-      <Box className="flex flex-row items.center justify-center">
-        <Heading className="text-xl" size="sm">
-          Users:
-        </Heading>
-        <Flex className="flex-row flex-wrap justify-center">
-          {users.map((user) => (
-            <UserCard key={user.id} user={user} />
-          ))}
-        </Flex>
+      {/*User*/}
+      <Box position="absolute" top="1rem" right="1rem">
+        {user ? ( // Render user name if logged in
+          <Text color="black">Welcome, {user.name}!</Text>
+        ) : (
+          // Othwerwise render login form
+          <>
+            <Button onClick={() => setIsLoginFormVisible(!isLoginFormVisible)}>
+              {isLoginFormVisible ? "Close" : "Login"}{" "}
+              {/* Toggles the login form's visibility */}
+            </Button>
+            {isLoginFormVisible && (
+              <Box
+                p={4}
+                mt={4}
+                bg="white"
+                borderWidth="1px"
+                borderRadius="md"
+                boxShadow="md"
+              >
+                <LoginForm
+                  onLoginSuccess={handleLoginSuccess}
+                  onError={handleLoginError}
+                />
+              </Box>
+            )}
+          </>
+        )}
+      </Box>
+
+      <Box className="center-col">
+        {user && (
+          <Box>
+            <Heading className="text-xl" size="sm">
+              User:
+            </Heading>
+            <Flex className="center-row">
+              <UserCard user={user} />
+            </Flex>
+          </Box>
+        )}
       </Box>
     </div>
   );
