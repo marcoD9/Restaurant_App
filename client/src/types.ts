76c6535dac0e64c9cab76c132bacc9d981786b46
Marco <marco.dmail96@gmail.com>
2025-03-25 15:19:15 +0100

    Update types, add LoginResponse

--- client/src/types.ts
+++ client/src/types.ts
@@ -9,8 +9,14 @@ export interface Dish {
 export interface User {
   id: string;
   username: string;
-  password: string;
   name: string;
   email: string;
   phoneNumber: string;
+}
+
+export interface LoginResponse {
+  user: {
+    id: string;
+  };
+  token: string;
 }
