b7709c22e9959456cc12a201cfdbcd492e9134ad
Marco <marco.dmail96@gmail.com>
2025-03-26 17:37:09 +0100

    Update Navigation with AuthContext

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,44 +1,44 @@
 import { useState } from "react";
-import { Box, Icon } from "@chakra-ui/react";
-import { FaTimes, FaUser } from "react-icons/fa";
-import UserLogin from "@/pages/UserLogin";
-function Navigation() {
+import { Box, Icon, Text, Flex } from "@chakra-ui/react";
+import { FaUser } from "react-icons/fa";
+import { useAuth } from "../AuthContext";
+import UserLogin from "./UserLogin";
+
+const Navigation = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const { user } = useAuth();
 
   return (
     <Box
       bgColor={isClicked ? "gray.300" : "gray.200"}
-      p={isClicked ? 4 : 2}
-      width={isClicked ? "98vw" : "16"}
-      height={16}
+      p={4}
+      width={isClicked ? "auto" : "24"}
+      height="auto"
       transition="all 0.3s ease-in-out"
       display="flex"
       alignItems="center"
-      justifyContent={isClicked ? "space-between" : "center"}
+      justifyContent="center"
       position="fixed"
       right="1rem"
       top="1rem"
       borderRadius="md"
-      marginLeft={isClicked ? "1rem" : "0"}
+      boxShadow="md"
+      zIndex={9999}
     >
       {isClicked ? (
-        <>
-          <Icon
-            as={FaTimes}
-            aria-label="Chiudi"
-            onClick={() => setIsClicked(false)}
-            size="sm"
-            mr={4}
-          />
-          <Box flex="1">
-            <UserLogin />
-          </Box>
-        </>
+        <UserLogin onClose={() => setIsClicked(false)} />
+      ) : user ? (
+        <Flex align="center">
+          <Text mr={2} fontWeight="bold" color="black">
+            {user.name}
+          </Text>
+          <Icon as={FaUser} boxSize={6} onClick={() => setIsClicked(true)} />
+        </Flex>
       ) : (
         <Icon as={FaUser} boxSize={6} onClick={() => setIsClicked(true)} />
       )}
     </Box>
   );
-}
+};
 
 export default Navigation;
