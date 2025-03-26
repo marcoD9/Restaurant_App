2beb9150e31d09a63123eae44f6f42138906f6de
Marco <marco.dmail96@gmail.com>
2025-03-26 17:37:22 +0100

    Update main with AuthProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import { Provider } from "@/components/ui/provider";
+import { AuthProvider } from "@/AuthContext";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -6,7 +7,11 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
-      <App />
+      {" "}
+      <AuthProvider>
+        {" "}
+        <App />
+      </AuthProvider>
     </Provider>
   </React.StrictMode>
 );
