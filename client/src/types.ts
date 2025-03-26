14f7d2a35d00e3c52e717cd4aa32fe1c597518f5
Marco <marco.dmail96@gmail.com>
2025-03-26 17:26:12 +0100

    Update types, removed interface form components

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,9 +1,14 @@
+import { ReactNode } from "react";
+
 export interface Dish {
   id: string;
   name: string;
   description: string;
   price: number;
   image: string;
+}
+export interface DishDetailsProps {
+  id: string;
 }
 
 export interface User {
@@ -14,9 +19,26 @@ export interface User {
   phoneNumber: string;
 }
 
+export interface LoginFormProps {
+  onLoginSuccess: (userData: LoginResponse) => Promise<void>;
+  onError: (error: string) => void;
+}
+
 export interface LoginResponse {
   user: {
     id: string;
   };
   token: string;
+}
+
+export interface AuthProviderProps {
+  children: ReactNode;
+}
+export interface AuthContextType {
+  user: User | null;
+  token: string | null;
+  login: (username: string, password: string) => Promise<void>;
+  logout: () => void;
+  setUser: (user: User | null) => void;
+  onError: (message: string) => void;
 }
