2de0fb5f99e4646de50f9eb1a1b051b7f8fe5fe7
Marco <marco.dmail96@gmail.com>
2025-03-28 20:56:08 +0100

    Update types

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,14 +1,9 @@
-import { ReactNode } from "react";
-
 export interface Dish {
   id: string;
   name: string;
   description: string;
   price: number;
   image: string;
-}
-export interface DishDetailsProps {
-  id: string;
 }
 
 export interface User {
@@ -19,30 +14,9 @@ export interface User {
   phoneNumber: string;
 }
 
-export interface LoginFormProps {
-  onLoginSuccess: (userData: LoginResponse) => Promise<void>;
-  onError: (error: string) => void;
-}
-
 export interface LoginResponse {
   user: {
     id: string;
   };
   token: string;
-}
-
-export interface UserLoginProps {
-  onClose: () => void;
-}
-
-export interface AuthProviderProps {
-  children: ReactNode;
-}
-export interface AuthContextType {
-  user: User | null;
-  token: string | null;
-  login: (username: string, password: string) => Promise<void>;
-  logout: () => void;
-  setUser: (user: User | null) => void;
-  onError: (message: string) => void;
 }
