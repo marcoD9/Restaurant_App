f5ec6b15ebd4e2c33bc0cae373d59ddeb46a6959
Marco <marco.dmail96@gmail.com>
2025-03-28 21:01:08 +0100

    Update main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { Provider } from "@/components/ui/provider";
-import { AuthProvider } from "@/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
