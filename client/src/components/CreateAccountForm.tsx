b40bd881bb29b65f2c51599c7453c3bfa5a564b9
Marco <marco.dmail96@gmail.com>
2025-03-30 18:23:57 +0200

    Create CreateAccountForm

---
+++ client/src/components/CreateAccountForm.tsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { Input, Button, VStack, Box } from "@chakra-ui/react";
+import { useAuth } from "@/contexts/AuthContext";
+
+const CreateAccountForm: React.FC = () => {
+  const { createUser } = useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await createUser(username, password, name, email, phoneNumber); // Use createUser function from authContext
+  };
+
+  return (
+    <Box width="300px" padding="20px" borderRadius="8px">
+      <form onSubmit={handleSubmit}>
+        <VStack>
+          <div>
+            <label
+              htmlFor="username"
+              style={{
+                display: "block",
+                marginBottom: "4px",
+                color: "black",
+              }}
+            >
+              Username
+            </label>
+            <Input
+              type="text"
+              id="username"
+              placeholder="Username"
+              className="text-color-primary"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              style={{
+                display: "block",
+                marginBottom: "4px",
+                color: "black",
+              }}
+            >
+              Password
+            </label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="text-color-primary"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="name"
+              style={{
+                display: "block",
+                marginBottom: "4px",
+                color: "black",
+              }}
+            >
+              Name
+            </label>
+            <Input
+              type="text"
+              id="name"
+              placeholder="Name"
+              className="text-color-primary"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>{" "}
+          <div>
+            <label
+              htmlFor="email"
+              style={{
+                display: "block",
+                marginBottom: "4px",
+                color: "black",
+              }}
+            >
+              Email
+            </label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="text-color-primary"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="phoneNumber"
+              style={{
+                display: "block",
+                marginBottom: "4px",
+                color: "black",
+              }}
+            >
+              Phone Number
+            </label>
+            <Input
+              type="text"
+              id="phoneNumber"
+              placeholder="Phone Number"
+              className="text-color-primary"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+          <Button className="text-color-primary" width="full" type="submit">
+            Create Account
+          </Button>
+        </VStack>
+      </form>
+    </Box>
+  );
+};
+
+export default CreateAccountForm;
