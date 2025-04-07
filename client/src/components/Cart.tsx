54d624b71964ce092b599c6a1f8a68b0a3666a4c
Marco <marco.dmail96@gmail.com>
2025-04-07 11:50:01 +0200

    Update Cart, now thr quantity can be selected

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCart } from "../contexts/CartContext";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Input } from "@chakra-ui/react";
 
 interface CartProps {
   dishId: string;
 }
 
 const Cart: React.FC<CartProps> = ({ dishId }) => {
-  const { addToCart, removeFromCart, decreaseQuantity } = useCart();
+  const { addToCart, removeFromCart } = useCart();
+  const [quantity, setQuantity] = useState(1); // State for quantity
 
   const handleAddToCart = () => {
-    addToCart(dishId, 1);
+    addToCart(dishId, quantity);
   };
 
   const handleRemoveFromCart = () => {
     removeFromCart(dishId);
   };
 
-  const handleDecreaseQuantity = () => {
-    decreaseQuantity(dishId);
-  };
-
   return (
     <Box display="flex" flexDirection="row" p={4} gap={4}>
+      <Input
+        color="black"
+        type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(parseInt(e.target.value))}
+        min={1}
+        max={100}
+        width="80px"
+      />
       <Button onClick={handleAddToCart}>Add</Button>
-      <Button onClick={handleDecreaseQuantity}>Decrease</Button>
-      <Button onClick={handleRemoveFromCart}>Remove</Button>
+      <Button onClick={handleRemoveFromCart}>Remove Item</Button>
     </Box>
   );
 };
