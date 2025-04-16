c2abb83bf1c514cdce190402203fc2439678b239
Marco <marco.dmail96@gmail.com>
2025-04-17 00:01:28 +0200

    Create Contact page

---
+++ client/src/pages/Contact.tsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+
+const Contact: React.FC = () => {
+  return (
+    <>
+      <Text fontSize="2xl" textAlign="center" color="black" mt={4}>
+        Contact Us at:
+      </Text>
+    </>
+  );
+};
+
+export default Contact;
