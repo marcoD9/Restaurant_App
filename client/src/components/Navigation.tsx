ee449cc50812a34b51f88b630f87163accbff579
Marco <marco.dmail96@gmail.com>
2025-04-22 13:07:28 +0200

    Update Nav bar, create an icon for smaller screen

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,17 +1,97 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Stack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Navigation = () => {
   const navigate = useNavigate();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
-    <Box w="100%" bg="gray.100" p={4} position="relative">
-      <Flex justifyContent="space-around" alignItems="center">
-        <Button onClick={() => navigate("/")}>Home</Button>
-        <Button onClick={() => navigate("/account")}>Account</Button>
-        <Button onClick={() => navigate("/checkout")}>Cart</Button>
-        <Button onClick={() => navigate("/contact")}>Contact</Button>
+    <Box w="full" bg="white" p={4} position="sticky" top={0} zIndex={10}>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Button
+          onClick={() => navigate("/")}
+          style={{
+            backgroundImage:
+              "linear-gradient(to right, green 33%, white 33% 66%, red 66%)",
+            padding: "8px",
+          }}
+        >
+          <Box padding={{ base: 2, md: 8 }}>
+            <Heading
+              as="h1"
+              size={{ base: "xl", md: "2xl", lg: "4xl" }}
+              fontWeight="bold"
+              mb={0}
+              textShadow="2px 2px rgba(255, 255, 255, 1.0)"
+              className="text-black"
+            >
+              LITTLE ITALY
+            </Heading>
+          </Box>
+        </Button>
+
+        {/* Button for mobile menu */}
+        <Button
+          display={{ base: "block", md: "none" }}
+          onClick={toggleMobileMenu}
+          aria-label={isMobileMenuOpen ? "Open Menu" : "Close Menu"}
+          bg="black"
+          color="white"
+        >
+          {isMobileMenuOpen ? "CLOSE" : "MENU"}
+        </Button>
+
+        {/* Nav links */}
+        <Stack
+          direction="row"
+          align="center"
+          display={{ base: "none", md: "flex" }}
+          ml={4}
+        >
+          <Button bg="black" color="white" onClick={() => navigate("/account")}>
+            ACCOUNT
+          </Button>
+          <Button
+            bg="black"
+            color="white"
+            onClick={() => navigate("/checkout")}
+          >
+            CART
+          </Button>
+          <Button bg="black" color="white" onClick={() => navigate("/contact")}>
+            CONTACT
+          </Button>
+        </Stack>
       </Flex>
+
+      {/* Menu */}
+      {isMobileMenuOpen && (
+        <Stack
+          mt={4}
+          align="center"
+          display={{ base: "flex", md: "none" }}
+          direction="column"
+        >
+          <Button bg="black" color="white" onClick={() => navigate("/account")}>
+            ACCOUNT
+          </Button>
+          <Button
+            bg="black"
+            color="white"
+            onClick={() => navigate("/checkout")}
+          >
+            CART
+          </Button>
+          <Button bg="black" color="white" onClick={() => navigate("/contact")}>
+            CONTACT
+          </Button>
+        </Stack>
+      )}
     </Box>
   );
 };
