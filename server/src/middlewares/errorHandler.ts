692e5df641c9d8fb0343221aae339763c9047107
Marco <marco.dmail96@gmail.com>
2025-04-23 12:23:53 +0200

    Update errorHandler, add next parameter

--- server/src/middlewares/errorHandler.ts
+++ server/src/middlewares/errorHandler.ts
@@ -1,6 +1,12 @@
-import { Request, Response } from "express";
-const errorHandler = (err: Error, req: Request, res: Response) => {
-  console.log(req.query); // use req to avoid error for unused variables
+import { Request, Response, NextFunction } from "express";
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  console.log(req.query);
   console.error(err);
   res.status(500).json({ message: "Something went wrong" });
 };
